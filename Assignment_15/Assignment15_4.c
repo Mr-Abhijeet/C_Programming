f67d333a0bb202ee38cce2bed898307f19129247
Abhijeet Lipane <abhijitlipane201@gmail.com>
2025-11-18 01:12:36 +0530

    Add files via upload

---
+++ Assignment_15/Assignment15_4.c
@@ -0,0 +1,29 @@
+int MultDigits(int iNo)
+{
+    int iMult = 1;
+    if(iNo == 0)
+        return 0;
+    while(iNo != 0)
+    {
+        iMult = iMult * (iNo % 10);
+        iNo = iNo / 10;
+    }
+    return iMult;
+}
+int main()
+{ 
+    int iValue = 0;
+
+    int iRet = 0;
+
+    printf("Enter number");
+
+    scanf("%d",&iValue);
+
+    iRet = MultDigits(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+
+}
